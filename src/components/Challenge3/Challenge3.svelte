a90e40a588e919ad3f93300200f64f99db0fd814
Kristin Baumann <hello@kristin-baumann.com>
2024-02-09 15:03:38 +0100

    Added interaction

--- src/components/Challenge3/Challenge3.svelte
+++ src/components/Challenge3/Challenge3.svelte
@@ -5,12 +5,12 @@
 	import { format } from 'd3-format';
 
 	const height = 550;
-	const width = 400;
+	const width = 450;
 	const margin = {
 		top: 0,
-		right: 20,
+		right: 10,
 		bottom: 0,
-		left: 40
+		left: 25
 	};
 	const barHeight = 14;
 
@@ -21,31 +21,43 @@
 		.domain([0, max(data, (d) => d.value)])
 		.range([0, innerWidth]);
 	const yScale = scaleLinear().domain([0, data.length]).range([0, innerHeight]);
+
+	$: hoveredIndex = null;
 </script>
 
 <h2 class="headline">Acres of Land owned by Negroes <br />in Georgia.</h2>
 <svg {width} {height}>
-	<g transform="translate({margin.left}, {margin.top})">
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<g
+		transform="translate({margin.left}, {margin.top})"
+		on:mouseout={() => (hoveredIndex = null)}
+		on:blur={() => (hoveredIndex = null)}
+	>
 		{#each data as d, i}
-			<rect x={xScale(0)} y={yScale(i)} height={barHeight} width={xScale(d.value)} />
-			<text
-				x={xScale(0)}
-				y={yScale(i) + barHeight / 2}
-				dx={-3}
-				dy={1}
-				text-anchor="end"
-				dominant-baseline="middle"
-				class="yearLabel">{d.year}</text
-			>
-			{#if d.year === 1874 || d.year === 1899}
+			<!-- svelte-ignore a11y-no-static-element-interactions -->
+			<g class="row" on:mouseover={() => (hoveredIndex = i)} on:focus={() => (hoveredIndex = i)}>
+				<rect x={xScale(0)} y={yScale(i)} height={barHeight} width={xScale(d.value)} />
+				<text
+					x={xScale(0)}
+					y={yScale(i) + barHeight / 2}
+					dx={-3}
+					dy={1}
+					text-anchor="end"
+					dominant-baseline="middle"
+					class="yearLabel {hoveredIndex === i ? 'active' : ''}">{d.year}</text
+				>
+				<!-- {#if d.year === 1874 || d.year === 1899 || i === hoveredIndex} -->
 				<text
 					x={xScale(d.value) / 2}
 					y={yScale(i) + barHeight / 2}
 					text-anchor="middle"
 					dominant-baseline="middle"
-					class="valueLabel">{format(',d')(d.value)}</text
+					class="valueLabel {d.year === 1874 || d.year === 1899 || hoveredIndex === i
+						? 'active'
+						: ''}">{format(',d')(d.value)}</text
 				>
-			{/if}
+				<!-- {/if} -->
+			</g>
 		{/each}
 	</g>
 </svg>
@@ -55,15 +67,32 @@
 		display: block;
 		margin: auto;
 	}
+	g.row {
+		cursor: pointer;
+	}
 	rect {
 		fill: #d22a49;
-		cursor: pointer;
+		transition: all 0.5s ease;
+	}
+	rect:hover {
+		fill-opacity: 0.6;
 	}
 	text {
 		font-size: 0.6rem;
+		transition: all 0.5s ease;
+	}
+	text.yearLabel {
 		font-weight: 200;
+	}
+	text.yearLabel.active {
+		font-weight: 700;
 	}
 	text.valueLabel {
 		font-weight: 700;
+		pointer-events: none;
+		opacity: 0;
+	}
+	text.valueLabel.active {
+		opacity: 1;
 	}
 </style>
