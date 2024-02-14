7788708e429bb4ebff7c968312a4350e965f8195
Kristin Baumann <hello@kristin-baumann.com>
2024-02-14 13:03:42 +0100

    Added circle text

--- src/components/Challenge4/Challenge4.svelte
+++ src/components/Challenge4/Challenge4.svelte
@@ -3,6 +3,7 @@
 <script>
 	import { geoInterruptedMollweideHemispheres } from 'd3-geo-projection';
 	import { geoPath } from 'd3-geo';
+	import { path } from 'd3-path';
 	import { feature } from 'topojson-client';
 
 	import worldData from '../../data/challenge04/land-110m.json';
@@ -11,27 +12,48 @@
 	import pointLayer from '../../data/challenge04/points_export.json';
 	import star from '$lib/assets/star.svg';
 
-	const projection = geoInterruptedMollweideHemispheres();
-
 	const width = 500;
+	const margin = {
+		top: 10,
+		right: 0,
+		bottom: 10,
+		left: 0
+	};
+	const innerWidth = width - margin.left - margin.right;
+	const radius = innerWidth / 4;
+	const circleTextOffset = 6;
 
+	// map
+	const projection = geoInterruptedMollweideHemispheres();
 	const land = feature(worldData, worldData.objects.land);
-
 	const outline = {
 		type: 'Sphere'
 	};
 	function getHeight() {
-		const [[x0, y0], [x1, y1]] = geoPath(projection.fitWidth(width, outline)).bounds(outline);
+		const [[x0, y0], [x1, y1]] = geoPath(projection.fitWidth(innerWidth, outline)).bounds(outline);
 		const dy = Math.ceil(y1 - y0);
 		const l = Math.min(Math.ceil(x1 - x0), dy);
 		projection.scale((projection.scale() * (l - 1)) / l).precision(0.2);
 		return dy;
 	}
-	const height = getHeight();
-	const path = geoPath(projection);
+	const height = getHeight() + margin.top + margin.bottom;
+	const getPath = geoPath(projection);
 
+	// marker
 	const georgiaLocation = projection(pointLayer.features[0].geometry.coordinates);
 	const markerSize = 9;
+
+	// text
+	function getCircleTextPathLeft() {
+		const p = path();
+		p.arc(0, 0, radius + circleTextOffset, 1.5 * Math.PI, 0);
+		return p.toString();
+	}
+	function getCircleTextPathRight() {
+		const p = path();
+		p.arc(0, 0, radius + circleTextOffset, Math.PI, 1.5 * Math.PI);
+		return p.toString();
+	}
 </script>
 
 <div class="wrapper">
@@ -39,30 +61,63 @@
 		The Georgio Negro.<br />A social study<br />By<br />W. E. Burghardt Du Bois.
 	</h2>
 	<svg {height} {width} style="display: block;">
-		<defs>
-			<path id="outline" d={path(outline)} class="outline" />
-			<clipPath id="clip"><use xlink:href={'http://localhost:5173/#outline'} /></clipPath>
-		</defs>
-		<g clip-path="url({'http://localhost:5173/#clip'})">
-			<use xlink:href="http://localhost:5173/#outline" fill="#dcba9e" />
-			<path d={path(land)} class="world" />
-			{#each polygonLayers.features as layer}
-				<path d={path(layer)} class="layers {layer.properties.type}" />
-			{/each}
-			{#each lineLayers.features as layer}
-				<path d={path(layer)} class="lines" />
-			{/each}
-			<image
-				href={star}
-				height={markerSize}
-				width={markerSize}
-				x={georgiaLocation[0] - markerSize / 2 - 1}
-				y={georgiaLocation[1] - markerSize / 2 - 1}
-				dx={-2}
-			/>
+		<g transform="translate({margin.left},{margin.top})">
+			<defs>
+				<path id="outline" d={getPath(outline)} class="outline" />
+				<clipPath id="clip"><use xlink:href="#outline" /></clipPath>
+			</defs>
+			<g clip-path="url({'#clip'})">
+				<!-- background world -->
+				<use href="#outline" fill="#dcba9e" />
+				<!-- world -->
+				<path d={getPath(land)} class="world" />
+				<!-- polygons in America and Africa -->
+				{#each polygonLayers.features as layer}
+					<path d={getPath(layer)} class="layers {layer.properties.type}" />
+				{/each}
+				<!-- lines between continents -->
+				{#each lineLayers.features as layer}
+					<path d={getPath(layer)} class="lines" />
+				{/each}
+				<!-- marker -->
+				<image
+					href={star}
+					height={markerSize}
+					width={markerSize}
+					x={georgiaLocation[0] - markerSize / 2 - 1}
+					y={georgiaLocation[1] - markerSize / 2 - 1}
+					dx={-2}
+				/>
+			</g>
+			<!-- outline around two circles -->
+			<use href="#outline" fill="none" stroke="#000" />
 		</g>
-
-		<use xlink:href={'http://localhost:5173/#outline'} fill="none" stroke="#000" />
+		<g transform="translate({margin.left + radius},{margin.top + radius})">
+			<defs>
+				<path d={getCircleTextPathLeft()} id="text-path-left-circle" stroke="red" fill="none" />
+			</defs>
+			<text
+				><textPath
+					href="#text-path-left-circle"
+					class="circle-text"
+					startOffset="45%"
+					text-anchor="middle">Distribution of</textPath
+				></text
+			>
+		</g>
+		<g transform="translate({margin.left + 3 * radius},{margin.top + radius})">
+			<defs>
+				<path d={getCircleTextPathRight()} id="text-path-right-circle" stroke="red" fill="none" />
+			</defs>
+			<text
+				><textPath
+					href="#text-path-right-circle"
+					class="circle-text"
+					startOffset="55%"
+					text-anchor="middle">the negro race</textPath
+				></text
+			>
+		</g>
 	</svg>
 
 	<p>
@@ -106,5 +161,10 @@
 	path.lines {
 		fill: none;
 		stroke: #333;
+	}
+	textPath.circle-text {
+		fill: #333;
+		font-size: 0.5rem;
+		text-transform: uppercase;
 	}
 </style>
