5111d9e516d2f8841be342e4049bed952df4e775
Kristin Baumann <hello@kristin-baumann.com>
2024-02-15 09:48:02 +0100

    Styling challenge 5

--- src/components/Challenge5/Challenge5.svelte
+++ src/components/Challenge5/Challenge5.svelte
@@ -5,27 +5,33 @@
 		{
 			type: 'black',
 			value: 44,
-			label: 'I.E. full-blooded negroes.'
+			label: ['IE. full-blooded', 'negroes.']
 		},
 		{
 			type: 'brown',
 			value: 40,
-			label: 'i.e. persons with some white blood or descendants of light colored africans'
+			label: [
+				'ie. persons with',
+				'some white blood',
+				'or descendants',
+				'of light colored',
+				' africans'
+			]
 		},
 		{
 			type: 'yellow',
 			value: 16,
-			label: 'i.e. persons with more with white than negro blood'
+			label: ['ie. persons with', 'more with white than', 'negro blood']
 		}
 	];
 
 	const height = 620;
-	const width = 390;
+	const width = 355;
 	const margin = {
 		top: 30,
 		right: 0,
 		bottom: 10,
-		left: 190
+		left: 150
 	};
 	const innerHeight = height - margin.top - margin.bottom;
 	const barWidth = 200;
@@ -49,8 +55,14 @@
 			>
 				<rect x={0} y={0} width={barWidth} height={yScale(d.value)} />
 				<text x={barWidth / 2} y={yScale(d.value) / 2} class="valueLabel">{d.value}%</text>
-				<text x={-margin.left} y={60} class="typeLabel">{d.type}.</text>
-				<text x={-margin.left} y={80} class="descriptionLabel">{d.label}</text>
+				<text x={-margin.left} y={40} class="typeLabel">{d.type}.</text>
+				<foreignObject x={-margin.left + 30} y={45} width={130} height={200}>
+					<p class="descriptionLabel" xmlns="http://www.w3.org/1999/xhtml">
+						{#each d.label as l}
+							{l}<br />
+						{/each}
+					</p>
+				</foreignObject>
 			</g>
 		{/each}
 	</g>
@@ -60,7 +72,6 @@
 	svg {
 		display: block;
 		margin: auto;
-		background-color: lightgray;
 	}
 	.valueLabel {
 		text-anchor: middle;
@@ -71,11 +82,17 @@
 	.brown text.valueLabel {
 		fill: darkred;
 	}
+	.black rect {
+		fill: #000;
+		stroke: #000;
+	}
 	.brown rect {
 		fill: #3a2117;
+		stroke: #3a2117;
 	}
 	.yellow rect {
 		fill: #f9ba00;
+		stroke: #f9ba00;
 	}
 	.typeLabel {
 		text-transform: uppercase;
@@ -84,5 +101,7 @@
 	.descriptionLabel {
 		text-transform: uppercase;
 		font-size: 0.6rem;
+		font-weight: 200;
+		margin-top: 0;
 	}
 </style>
