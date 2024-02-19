ccba00b82ab74cce915773fb68095556ec3f657e
Kristin Baumann <hello@kristin-baumann.com>
2024-02-19 10:29:07 +0100

    Challenge 6: Added year label and lines

--- src/components/Challenge6/Challenge6.svelte
+++ src/components/Challenge6/Challenge6.svelte
@@ -18,9 +18,9 @@
 	const width = 520;
 	const height = 590;
 	const margin = {
-		top: 20,
+		top: 40,
 		right: 20,
-		bottom: 0,
+		bottom: 20,
 		left: 20
 	};
 	const innerHeight = height - margin.top - margin.bottom;
@@ -33,55 +33,85 @@
 	const wBlack = 84;
 	const wSum = wWhite + wLightYellow + wDarkYellow + wBrown + wBlack;
 
-	const xPercFactor1800 = 500000 / 99; // adjust
-	const xPercFactor1840 = 2000000 / 95; // adjust
-	const xPercFactor1860 = 3542147 / 90;
-	const xPercFactor1890 = 6337980 / 85;
+	const xPercFactor1890 = 6337980 / 84;
+
+	// x scale from right to left
 	const xScale = scaleLinear()
 		.domain([0, xPercFactor1890 * wSum])
 		.range([innerWidth, 0]);
 
-	// tip of the triangles, in 1800, same as meeting point between brown and dark yellow in 1890
-	const triangleTipX =
-		innerWidth - xScale(xPercFactor1890 * (wSum - wWhite - wLightYellow - wDarkYellow));
+	// y scale from top to bottom (not based on years to recreate skewed axis)
+	const yScale = scaleLinear().domain([1, 4]).range([0, innerHeight]);
 
+	/**
+	 * pre-calculated points
+	 */
+	const y1890 = yScale(4);
+	// > black
+	const xBlackLeft1890 =
+		innerWidth -
+		xScale(xPercFactor1890 * (wSum - wWhite - wLightYellow - wDarkYellow - wBrown - wBlack));
+	const xBlackLeft1860 = innerWidth - xScale(xPercFactor1890 * 30);
+	const xBlackLeft1840 = innerWidth - xScale(xPercFactor1890 * 50);
+	const xBlackLeft1800 = innerWidth - xScale(xPercFactor1890 * 80);
+	// > brown
+	const xBrownLeft1890 =
+		innerWidth - xScale(xPercFactor1890 * (wSum - wWhite - wLightYellow - wDarkYellow - wBrown));
+	// > dark yellow
+	const xDarkYellowLeft1890 =
+		innerWidth - xScale(xPercFactor1890 * (wSum - wWhite - wLightYellow - wDarkYellow));
+	// > light yellow
+	const xLightYellowLeft1890 =
+		innerWidth - xScale(xPercFactor1890 * (wSum - wWhite - wLightYellow));
+	// > white
+	const xWhiteLeft1890 = innerWidth - xScale(xPercFactor1890 * (wSum - wWhite));
+	// > tip of the triangles, in 1800, same as meeting point between brown and dark yellow in 1890
+	const triangleTip = {
+		x: xDarkYellowLeft1890,
+		y: yScale(1)
+	};
+
+	/**
+	 * polygon paths
+	 */
+
+	// > brown counter clock wise
 	function drawPathBrown() {
 		const pa = path();
-		const topPoint = {
-			x: triangleTipX,
-			y: 50
-		};
-		pa.moveTo(topPoint.x, topPoint.y);
-		pa.lineTo(topPoint.x, 540);
-		pa.lineTo(
-			innerWidth - xScale(xPercFactor1890 * (wSum - wWhite - wLightYellow - wDarkYellow - wBrown)),
-			540
-		);
+		pa.moveTo(triangleTip.x, triangleTip.y);
+		pa.lineTo(triangleTip.x, y1890);
+		pa.lineTo(xBrownLeft1890, y1890);
 		pa.closePath();
 		return pa.toString();
 	}
+	// > dark yellow clock wise
 	function drawPathDarkYellow() {
 		const pa = path();
-		const topPoint = {
-			x: triangleTipX,
-			y: 50
-		};
-		pa.moveTo(topPoint.x, topPoint.y);
-		pa.lineTo(topPoint.x, 540);
-		pa.lineTo(innerWidth - xScale(xPercFactor1890 * (wSum - wWhite - wLightYellow)), 540);
+		pa.moveTo(triangleTip.x, triangleTip.y);
+		pa.lineTo(triangleTip.x, y1890);
+		pa.lineTo(xLightYellowLeft1890, y1890);
 		pa.closePath();
 		return pa.toString();
 	}
+	// > light yellow clock wise
 	function drawPathLightYellow() {
 		const pa = path();
-		const topPoint = {
-			x: triangleTipX,
-			y: 50
-		};
-		pa.moveTo(topPoint.x, topPoint.y);
-		pa.lineTo(innerWidth - xScale(xPercFactor1890 * (wSum - wWhite - wLightYellow)), 540);
-		pa.lineTo(innerWidth - xScale(xPercFactor1890 * (wSum - wWhite)), 540);
+		pa.moveTo(triangleTip.x, triangleTip.y);
+		pa.lineTo(xLightYellowLeft1890, y1890);
+		pa.lineTo(xWhiteLeft1890, y1890);
 		pa.closePath();
+		return pa.toString();
+	}
+	// > black counter clock wise
+	function drawPathBlack() {
+		const pa = path();
+		pa.moveTo(triangleTip.x, triangleTip.y); // start at triangle tip
+		pa.lineTo(xBlackLeft1800, yScale(1)); // to left in 1800
+		pa.lineTo(xBlackLeft1840, yScale(2)); // to down left in 1840
+		pa.lineTo(xBlackLeft1860, yScale(3)); // to down left in 1860
+		pa.lineTo(xBlackLeft1890, y1890); // to down left in 1890
+		pa.lineTo(xBrownLeft1890, y1890); // to right in 1890
+		pa.closePath(); // back up to triangle tip
 		return pa.toString();
 	}
 </script>
@@ -93,64 +123,24 @@
 </h2>
 <svg {width} {height}>
 	<g transform="translate({margin.left}, {margin.top})">
-		<!-- white -->
-		<rect
-			x={innerWidth - xScale(xPercFactor1890 * (wSum - wWhite))}
-			y={540}
-			width={innerWidth - xScale(xPercFactor1890 * wWhite)}
-			height={10}
-			fill="#d1c6ba"
-		/>
-		<!-- light yellow -->
-		<rect
-			x={innerWidth - xScale(xPercFactor1890 * (wSum - wWhite - wLightYellow))}
-			y={540}
-			width={innerWidth - xScale(xPercFactor1890 * wLightYellow)}
-			height={10}
-			fill="#e7ca87"
-		/>
-		<!-- // dark yellow -->
-		<rect
-			x={innerWidth - xScale(xPercFactor1890 * (wSum - wWhite - wLightYellow - wDarkYellow))}
-			y={540}
-			width={innerWidth - xScale(xPercFactor1890 * wDarkYellow)}
-			height={10}
-			fill="#f4ad00"
-		/>
-		<!-- brown -->
-		<rect
-			x={innerWidth -
-				xScale(xPercFactor1890 * (wSum - wWhite - wLightYellow - wDarkYellow - wBrown))}
-			y={540}
-			width={innerWidth - xScale(xPercFactor1890 * wBrown)}
-			height={10}
-			fill="#895c38"
-		/>
-		<rect
-			x={innerWidth -
-				xScale(xPercFactor1890 * (wSum - wWhite - wLightYellow - wDarkYellow - wBrown - wBlack))}
-			y={540}
-			width={innerWidth - xScale(xPercFactor1890 * wBlack)}
-			height={10}
-			fill="black"
-		/>
-		<rect
-			x={innerWidth - xScale(xPercFactor1890 * (wSum - wWhite - wLightYellow - wDarkYellow))}
-			y={50}
-			width={innerWidth - xScale(xPercFactor1890 * (wWhite + wLightYellow + wDarkYellow))}
-			height={10}
-			fill="#d1c6ba"
-		/>
-		<rect
-			x={innerWidth - xScale(xPercFactor1890 * (wSum - wWhite - wLightYellow - wDarkYellow - 20))}
-			y={50}
-			width={innerWidth - xScale(xPercFactor1890 * 20)}
-			height={10}
-			fill="black"
-		/>
-		<path d={drawPathBrown()} fill="#895c38" />
-		<path d={drawPathDarkYellow()} fill="#f4ad00" />
-		<path d={drawPathLightYellow()} fill="#e7ca87" />
+		<g class="polygons">
+			<path d={drawPathBrown()} fill="#895c38" />
+			<path d={drawPathDarkYellow()} fill="#f4ad00" />
+			<path d={drawPathLightYellow()} fill="#e7ca87" />
+			<path d={drawPathBlack()} fill="black" />
+		</g>
+		<g class="yearLabels" dominant-baseline="middle">
+			<text x={xBlackLeft1890} y={yScale(4)} id="yearLabel1890" dy={8}>1890</text>
+			<text x={xBlackLeft1860} y={yScale(3)} dx={-8}>1860</text>
+			<text x={xBlackLeft1840} y={yScale(2)} dx={-8}>1840</text>
+			<text x={xBlackLeft1800} y={yScale(1)} dx={-8}>1800</text>
+		</g>
+		<g class="yearLines">
+			<line x1={xScale(0)} y1={yScale(1)} x2={xBlackLeft1800} y2={yScale(1)}></line>
+			<line x1={xScale(0)} y1={yScale(2)} x2={xBlackLeft1840} y2={yScale(2)}></line>
+			<line x1={xScale(0)} y1={yScale(3)} x2={xBlackLeft1860} y2={yScale(3)}></line>
+			<line x1={xScale(0)} y1={yScale(4)} x2={xBlackLeft1890} y2={yScale(4)}></line>
+		</g>
 	</g>
 </svg>
 
@@ -159,5 +149,16 @@
 		display: block;
 		margin: 70px 0 0 auto;
 		background-color: rgba(211, 211, 211, 0.5);
+	}
+	.yearLabels text {
+		text-anchor: end;
+		font-size: 0.7rem;
+	}
+	.yearLabels text#yearLabel1890 {
+		text-anchor: start;
+	}
+	.yearLines line {
+		stroke: white;
+		stroke-width: 1;
 	}
 </style>
