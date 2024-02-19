a4977e17d0c6f9b1bdfc339148c3305f834d1029
Kristin Baumann <hello@kristin-baumann.com>
2024-02-19 14:09:53 +0100

    Added challenge 7

--- src/components/Challenge7/Challenge7.svelte
+++ src/components/Challenge7/Challenge7.svelte
@@ -1,8 +1,87 @@
+<!-- 
+	From: https://github.com/ajstarks/dubois-data-portraits/blob/master/challenge/2024/challenge07/Note
+This chart has no units, so extrapilating the illiteracy rate of Black
+Americans in the 1890 Census (56.8%, source "Excerpts are taken from
+Chapter 1 of 120 Years of American Education: A Statistical Portrait
+(Edited by Tom Snyder, National Center for Education Statistics,
+1993).", https://nces.ed.gov/naal/lit_history.asp).
+
+56.8% -- 8.8cm on the chart yields 6.45454 illitercy points/cm. by
+measuring the length of each bar (recorded in ill.d), you can compute
+the illteracy points.
+-->
+
 <script>
+	import { scaleLinear } from 'd3-scale';
+	import data from '../../data/challenge07/data.js';
+
+	const height = 520;
+	const width = 500;
+	const margin = {
+		top: 50,
+		right: 20,
+		bottom: 45,
+		left: 80
+	};
+	const barHeight = 20;
+	const innerHeight = height - margin.top - margin.bottom;
+	const innerWidth = width - margin.left - margin.right;
+
+	const xScale = scaleLinear().domain([0, 73]).range([0, innerWidth]);
+	const yScale = scaleLinear()
+		.domain([0, data.length - 1])
+		.range([0, innerHeight]);
 </script>
 
 <h2 class="headline">
-	<span class="subtitle"
-		>Illiteracy of the American Negroes compared with that of other nations.</span
-	>
+	<p>Illiteracy of the American Negroes compared with that of other nations.</p>
+	<p>Proportion d'illettrés parmi les Nègres Americains comparée à celle des autres nations.</p>
+	<p>Done by Atlanta University.</p>
 </h2>
+<svg {width} {height}>
+	<g transform="translate({margin.left}, {margin.top})">
+		{#each data as d, i}
+			<rect
+				x={xScale(0)}
+				y={yScale(i)}
+				width={xScale(d.value)}
+				height={barHeight}
+				class={d.country === 'Negroes, U.S.A.' ? 'highlight' : ''}
+			/>
+			<text x={-margin.left} y={yScale(i) + barHeight / 2} dominant-baseline="central"
+				>{d.country}</text
+			>
+		{/each}
+	</g>
+</svg>
+
+<style>
+	.headline p {
+		font-weight: 400;
+		text-transform: none;
+		margin-top: 0;
+		font-family: 'Times New Roman', Times, serif;
+		color: #333;
+	}
+	.headline p:nth-of-type(2) {
+		font-size: 0.8rem;
+	}
+	.headline p:nth-of-type(3) {
+		font-size: 0.6rem;
+	}
+	svg {
+		display: block;
+		margin-top: 50px;
+	}
+	rect {
+		fill: #1e7c4d;
+		stroke: black;
+		stroke-width: 0.5px;
+	}
+	rect.highlight {
+		fill: #c31f3d;
+	}
+	text {
+		font-size: 0.6rem;
+	}
+</style>
---
+++ src/data/challenge07/data.js
@@ -0,0 +1,13 @@
+const data = [
+	{ country: 'Romaine', value: 72.93639 },
+	{ country: 'Servie', value: 72.29089 },
+	{ country: 'Russie', value: 72.29089 },
+	{ country: 'Negroes, U.S.A.', value: 56.89 },
+	{ country: 'Hongrie', value: 55.5099 },
+	{ country: 'Italie', value: 43.89099 },
+	{ country: 'Autriche', value: 34.85459 },
+	{ country: 'Ireland', value: 25.17279 },
+	{ country: 'France', value: 12.90919 },
+	{ country: 'Suede', value: 0.6454549 }
+];
+export default data;
