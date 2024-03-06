fc19ce0229e143b22b2718a116144ad3c20d859a
Kristin Baumann <hello@kristin-baumann.com>
2024-03-06 14:37:40 +0100

    Added color to map for challenge 10

--- src/components/Challenge10/Map.svelte
+++ src/components/Challenge10/Map.svelte
@@ -1,28 +1,66 @@
 <script>
 	import { geoPath, geoAlbersUsa } from 'd3-geo';
 	import mapGeo from '../../data/challenge10/states-20m-geo.json';
+	import mapData from '../../data/challenge10/map_data.json';
 
-	const height = 250;
-	const width = 300;
+	const height = 300;
+	const width = 500;
 
 	const margin = { top: 20, right: 20, bottom: 20, left: 20 };
 
-	let projection = geoAlbersUsa();
-	projection.fitSize([width, height], mapGeo);
-	let drawPath = geoPath(projection);
+	// create a unit projection
+	let projection = geoAlbersUsa().scale(1).translate([0, 0]);
 
-	$: console.log(mapGeo);
+	// create a path generator
+	let path = geoPath(projection);
+
+	// Compute the bounds of the map, then derive scale & translate.
+	const b = path.bounds(mapGeo),
+		s = 0.95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
+		t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+
+	// Update the projection to use computed scale & translate.
+	projection.scale(s).translate(t);
+
+	function getColor(stateName) {
+		const el = mapData.filter((el) => el.NAME === stateName);
+		if (el.length === 0) {
+			return 'black';
+		}
+		const colorPattern = {
+			A: 'white',
+			B: '#896557', // brown
+			BL: '#000', // black
+			LB: '#7a89b6', // light blue
+			DB: '#20266b', // dark blue
+			DG: 'darkgrey', // dark grey
+			LG: 'lightgrey', // light grey
+			R: '#e2415d', // red
+			P: '#f2c0b0', // pink
+			Y: '#fec45f' // yellow
+		};
+		return colorPattern[el[0].type];
+	}
 </script>
 
 <div class="chart-container-map">
 	<svg {width} {height}>
 		{#each mapGeo.features as c, i}
-			{#if c.properties.NAME !== 'Alaska' && c.properties.NAME !== 'Hawaii'}
-				<path d={drawPath(c)} data-label={c.properties.NAME} class="state" fill="black" />
+			{#if c.properties.NAME !== 'Alaska' && c.properties.NAME !== 'Hawaii' && c.properties.NAME !== 'Puerto Rico'}
+				<path
+					d={path(c)}
+					data-label={c.properties.NAME}
+					class={`state ${c.properties.NAME}`}
+					fill={getColor(c.properties.NAME)}
+				/>
 			{/if}
 		{/each}
 	</svg>
 </div>
 
 <style>
+	path.state {
+		stroke: #333;
+		stroke-width: 1;
+	}
 </style>
